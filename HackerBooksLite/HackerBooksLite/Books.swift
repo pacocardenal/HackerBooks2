e88ece635596591271cb14eee363ecafb2005234
Fernando Rodriguez <fernando@keepcoding.io>
2016-08-14 09:48:07 -0700

    first version of books

--- HackerBooksLite/HackerBooksLite/Books.swift
+++ HackerBooksLite/HackerBooksLite/Books.swift
@@ -16,12 +16,32 @@ typealias Authors = [Author]
 typealias Title = String
 typealias Tag = String
 typealias Tags = [Tag]
-typealias PDF = Data
+typealias PDF = AsyncData
+typealias Image = AsyncData
 
 class Book{
-    let authors : Authors
-    let title   : Title
-    let tags    : Tags
-    
+    let _authors : Authors
+    let _title   : Title
+    let _tags    : Tags
+    let _pdf     : PDF
+    let _image   : Image
 
+    init(title: Title, authors: Authors,
+         tags: Tags, pdf: PDF, image: Image) {
+        
+        (_title, _authors, _tags, _pdf, _image) = (title, authors, tags, pdf, image)
+        
+    }
+    
+    func formattedListOfAuthors() -> String{
+        
+        return _authors.sorted().joined(separator: ", ").capitalized
+        
+    }
+    
+    func formattedListOfTags() -> String{
+        return _tags.sorted().joined(separator: ", ").capitalized
+    }
+    
+    
 }
