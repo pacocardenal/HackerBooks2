8e8de2a6a6a2e81789440c8c6b2970c1b6474180
Fernando Rodriguez <fernando@keepcoding.io>
2016-08-14 17:10:28 -0700

    Tags reified

--- HackerBooksLite/HackerBooksLite/Tags.swift
+++ HackerBooksLite/HackerBooksLite/Tags.swift
@@ -15,7 +15,7 @@ struct TagConstants{
     static let favoriteTag = "Favorite"
 }
 
-struct Tag {
+struct Tag{
     
     let _name : String
     
@@ -27,4 +27,50 @@ struct Tag {
         return self.init(name: TagConstants.favoriteTag)
     }
     
+    func isFavorite()->Bool{
+        return _name == TagConstants.favoriteTag
+    }
 }
+
+//MARK: - Hashable
+// Since tags will go into a MultiDictionary, they must be hashable
+extension Tag: Hashable{
+    public var hashValue: Int {
+        return _name.hashValue
+    }
+}
+
+//MARK: - Equatable
+// To be hashable, you must be equatable
+// As of swift 3.0, operators can be declared in
+// extension
+extension Tag : Equatable{
+    static func ==(lhs: Tag, rhs: Tag) -> Bool{
+        return (lhs._name == rhs._name)
+    }
+}
+
+
+//MARK: - Comparable
+extension Tag: Comparable{
+    static func <(lhs: Tag, rhs: Tag) -> Bool{
+        
+        if lhs.isFavorite(){
+            return true
+        }else{
+            return lhs._name < rhs._name
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
